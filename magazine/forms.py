ef2d85ad3579ddcde0b4e0a92b92acc850774af7
iamenochchirima <iamenochchirima@gmail.com>
2023-02-21 12:55:16 +0200

    Updated models

---
+++ magazine/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from tinymce.widgets import TinyMCE
+from .models import Article
+
+
+class ArticleAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+
+    class Meta:
+        model = Article
+        fields = ['title', 'content', 'cover_image', 'category', 'slug', 'published', 'status', 'editor']
--- magazine/models.py
+++ magazine/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+def get_cover_image_filepath(self, filename):
+    return 'profile_images/' + str(self.pk) + '/cover_image.png'
+
+
+def default_cover_image():
+    return "default_image/default_cover_image.png"
+
+
+class Article(models.Model):
+
+    class ArticleObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset() .filter(status='published')
+
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    cover_image = models.ImageField(
+        upload_to=get_cover_image_filepath, null=True, blank=True, default=default_cover_image)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    slug = models.SlugField(max_length=250, unique_for_date='published')
+    published = models.DateTimeField(default=timezone.now)
+    status = models.CharField(
+        max_length=10, choices=options, default='published')
+    editor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='article')
+
+    objects = models.Manager()
+    postobjects = ArticleObjects()
+
+    class Meta:
+        ordering = ('-published',)
+
+    def __str__(self):
+        return self.title
