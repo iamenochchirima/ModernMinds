acc671819e13f3875d6d908067d8c56081aa0e39
iamenochchirima <iamenochchirima@gmail.com>
2023-03-03 11:35:56 +0200

    Paaword reset

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('create/', views.CustomUserCreate.as_view(), name="create_user"),
     path('load/', views.LoadUserView.as_view(), name='load-user'),
     path('verify-email/<str:uidb64>/<str:token>/', views.VerifyEmailView.as_view(), name='verify-email'),
+    path('password-reset/', views.PasswordResetRequestView.as_view(), name='password_reset'),
+    path('password-reset/confirm/<str:uidb64>/<str:token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
