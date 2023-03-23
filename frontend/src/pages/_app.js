9e339a30638f0156a38975edefaaaebd151a9ff6
iamenochchirima <iamenochchirima@gmail.com>
2023-03-23 19:56:12 +0200

    logoogle analytics

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,19 +1,55 @@
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { wrapper } from "../redux/Store";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
+import Head from "next/head";
 
 export default function App({ Component, ...rest }) {
   const { store, props } = wrapper.useWrappedStore(rest);
   const { pageProps } = props;
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
-    <Provider store={store}>
-      <div className="font-graphik">
-      <ToastContainer />
-      <Component {...pageProps} />
-    </div>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <div className="font-graphik">
+          <Head>
+            <Script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+              }}
+            />
+          </Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <ToastContainer />
+          <Component {...pageProps} />
+        </div>
+      </Provider>
+    </>
   );
 }
--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,15 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+
+import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="icon" href="/logo.png"/>
+        <link rel="icon" href="/logo.png" />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${GA_TRACKING_ID}');
+                    `,
+          }}
+        />
       </body>
     </Html>
-  )
+  );
 }
