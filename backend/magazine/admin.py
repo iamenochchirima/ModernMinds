7e45f2505408d9ca42e374a7dd08791c9a45b876
iamenochchirima <iamenochchirima@gmail.com>
2023-03-25 11:02:55 +0200

    loSpecial articles

--- backend/magazine/admin.py
+++ backend/magazine/admin.py
@@ -1,18 +1,10 @@
 from django.contrib import admin
-from .models import Article, Category, SpecialArticle
-from .forms import ArticleModelForm, SpecialArticleModelForm
+from .models import Article, Category
 
 @admin.register(Article)
 class ArticleModelAdmin(admin.ModelAdmin):
-    form = ArticleModelForm
     model = Article
     list_display = ('id', 'title', 'special', 'editor_note', 'todays_pick', 'top_story', 'archived', 'issue', 'category', 'created_at', 'status')
-
-@admin.register(SpecialArticle)
-class SpecialArticleModelAdmin(admin.ModelAdmin):
-    form = SpecialArticleModelForm
-    model = Article
-    list_display = ('id', 'title', 'editor_note', 'todays_pick', 'top_story', 'archive', 'category', 'issue', 'created_at', 'status')
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug', 'cover_image')
